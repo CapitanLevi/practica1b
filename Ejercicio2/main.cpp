da5ca10868ef74f8bfd70f81ec6bbf9f40514945
CapitanLevi <camilo.hernandez1@udea.edu.co>
2018-02-23 11:34:42 -0500

    Ejercicio 2 listo + descripción del programa

--- Ejercicio2/main.cpp
+++ Ejercicio2/main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+/* Ejercicio 2 de practica 1.
+ * El usuario ingresa un entero y el programa devuelve si es par o impar.
+ * Ejemplo:
+ * Escriba un numero: 5
+ * 5 es impar.
+*/
+
 int main()
 {
     int numero;
